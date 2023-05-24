52a394b15f157adb1340aa821f0b8290b8db0057
fnguedes <tututs0@academico.ufs.br>
2023-05-23 21:11:00 -0300

    Bug arrumado, não foi identificado, foi refeito

--- App.js
+++ App.js
@@ -1,31 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params'
-import Field from './src/components/Fields'
+import Field from './src/components/Field'
+import { Component } from 'react';
+import MineField from './src/components/MineField';
+import { createMineBoard } from './src/functions';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-        <Text>Iniciando mine</Text>
-        <Text>Tamanho da grade: {params.quantDeColuna()}x{params.quantDeLinhas()}</Text>
-        <Field/>
-        <Field opened/>
-        <Field opened nearMines={2}/>
-        <Field opened nearMines={3}/>
-        <Field opened nearMines={6}/>
-        <Field opened mined/>
-        <Field opened mined exploded/>
-        <Field flagged/>
-        <Field flagged opened/>
-    </View>
-  );
+export default class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = this.createState()
+    }
+
+    minesAmount = () => {
+        const cols = params.quantDeColuna()
+        const rows = params.quantDeLinhas()
+
+        return Math.ceil(cols * rows * params.difficultLevel)
+    }
+
+    createState = () => {
+        const cols = params.quantDeColuna()
+        const rows = params.quantDeLinhas()
+        return {
+            board: createMineBoard(rows, cols, this.minesAmount())
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Iniciando mine</Text>
+                <Text>Tamanho da grade: {params.quantDeColuna()}x{params.quantDeLinhas()}</Text>
+                <View style={styles.board}>
+                    <MineField board={this.state.board}/>
+                </View>
+            </View>
+        );
+    }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    board:{
+        alignItems:'center',
+        backgroundColor:'#aaa'
+    }
 });
--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,48 +1,70 @@
-import { View, Text ,StyleSheet} from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function Flag() {
+export default function Flag(props) {
   return (
     <View style={style.container}>
-      <View style={style.mastro}/>
-      <View style={style.base1}/>
-      <View style={style.base2}/>
-      <View style={style.bandeira}/>
+      <View style={[style.mastro, props.bigger ? style.mastroG : null]} />
+      <View style={[style.base1, props.bigger ? style.base1G : null]} />
+      <View style={[style.base2, props.bigger ? style.base2G : null]} />
+      <View style={[style.bandeira, props.bigger ? style.bandeiraG : null]} />
     </View>
   )
 }
-const style=StyleSheet.create({
-  container:{
-    marginTop:2
+const style = StyleSheet.create({
+  container: {
+    marginTop: 2
   },
-  mastro:{
-    position:'absolute',
-    height:14,
-    width:2,
-    backgroundColor:'#222',
-    marginLeft:9,
+  mastro: {
+    position: 'absolute',
+    height: 14,
+    width: 2,
+    backgroundColor: '#222',
+    marginLeft: 9,
   },
-  base1:{
-    position:'absolute',
-    height:2,
-    width:6,
-    backgroundColor:'#222',
-    marginLeft:7,
-    marginTop:10
+  mastroG: {
+    height: 28,
+    width: 4,
+    marginLeft: 16
   },
-  base2:{
-    position:'absolute',
-    height:2,
-    width:10,
-    backgroundColor:'#222',
-    marginLeft:5,
-    marginTop:12
+  base1: {
+    position: 'absolute',
+    height: 2,
+    width: 6,
+    backgroundColor: '#222',
+    marginLeft: 7,
+    marginTop: 10
   },
-  bandeira:{
-    position:'absolute',
-    height:5,
-    width:6,
-    backgroundColor:"#f22",
-    marginLeft:3
+  base1G: {
+    height: 4,
+    width: 12,
+    marginTop: 20,
+    marginLeft: 12
+  },
+  base2: {
+    position: 'absolute',
+    height: 2,
+    width: 10,
+    backgroundColor: '#222',
+    marginLeft: 5,
+    marginTop: 12
+  },
+  base2G: {
+    height: 4,
+    width: 20,
+    marginLeft: 8,
+    marginTop: 24
+  },
+  bandeira: {
+    position: 'absolute',
+    height: 5,
+    width: 6,
+    backgroundColor: "#f22",
+    marginLeft: 3
+  },
+  bandeiraG: {
+    height: 10,
+    width: 14,
+    marginLeft: 3
   }
 })
